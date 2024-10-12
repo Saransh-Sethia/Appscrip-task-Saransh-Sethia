3cb28d1716cc8260502a9b1af8ad384be9bd8338
saransh <sethia.saransh.ss@gmail.com>
2024-10-12 14:39:03 +0530

    seventh commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,46 @@
 import React from "react";
+import Logo from "../assets/Logo.png";
+import search from "../assets/search-normal.png";
+import heart from "../assets/heart.png";
+import profile from "../assets/profile.png";
+import bag from "../assets/shopping-bag.png";
+import element from "../assets/element-4.png";
 
 const Header = () => {
   return (
     <div className="main-header">
-      <div></div>
-      <div></div>
-      <div></div>
+      <div className="top-header">
+<div className="lorem-ipsum">
+  <img src={element} alt="element" />
+  <p>lorem ipsum dolor</p>
+</div>
+<div className="lorem-ipsum">
+  <img src={element} alt="element" />
+  <p>lorem ipsum dolor</p>
+</div>
+<div className="lorem-ipsum">
+  <img src={element} alt="element" />
+  <p>lorem ipsum dolor</p>
+</div>
+      </div>
+      <div className="mid-header">
+<img src={Logo} alt="logo"/>
+<h1>LOGO</h1>
+<div className="logos">
+<img src={search} alt="search"/>
+<img src={heart} alt="heart"/>
+<img src={bag} alt="bag"/>
+<img src={profile} alt="profile"/>
+
+</div>
+      </div>
+      <div className="lower-header">
+<h3>Shop</h3>
+<h3>Skills</h3>
+<h3>Stories</h3>
+<h3>About</h3>
+<h3>Contact Us</h3>
+      </div>
     </div>
   );
 };
